bacd9ed4d819ea06b86669e5c91766c122a7bf0e
Na0Se <minminnminm333@gmail.com>
2021-12-08 19:34:09 +0900

    edit show view

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,14 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('top');
+// この行を追記
+Route::group(['prefix' => 'users', 'middleware' => 'auth'], function () {
+    Route::get('show/{id}', 'UserController@show')->name('users.show');
 });
 
 Auth::routes();
 
+Route::get('/', function () {
+    return view('top');
+    });
+
 Route::get('/home', 'HomeController@index')->name('home');
-//$this->get('login', 'Auth\LoginController@showLoginForm')->name('login');
