3ed2b9c02726a7b71b33e30c3b72cc559a149134
Na0Se <minminnminm333@gmail.com>
2021-12-08 19:56:41 +0900

    edit shouw.blade

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\User;
+
 class UserController extends Controller
 {
     public function show($id)
     {
-        return view('users.show');
+        $user = User::findorFail($id);
+
+        //dd($user); 
+
+        return view('users.show', compact('user')); 
     }
 }
