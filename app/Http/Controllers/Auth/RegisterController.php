b641e62f136fc205bb1b03e81c130366a8b3e506
Na0Se <minminnminm333@gmail.com>
2021-12-08 19:15:58 +0900

    edit register view

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -53,8 +53,6 @@ class RegisterController extends Controller
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
-            'self_introduction' => $data['self_introduction'],
-            'type' => $data['type'],
         ]);
     }
 
@@ -70,6 +68,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'self_introduction' => $data['self_introduction'],
+            'sex' => $data['sex'],
         ]);
     }
 }
